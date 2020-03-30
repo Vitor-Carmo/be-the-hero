ffc9e535a5077991ff146b4cac7709a97b4529a5
Vitor Carmo <vitorv0071@gmail.com>
2020-03-30 02:11:54 -0300

    problema logon resolvido

--- frontend/src/pages/register/index.js
+++ frontend/src/pages/register/index.js
@@ -64,7 +64,7 @@ export default function Register(){
 
                     <Link  className="back-link" to="/">
                         <FiArrowLeft size={16} color="#e02041"/>
-                        Não tenho cadastro
+                        Voltar para o logon
                     </Link>
                 </section>
 
